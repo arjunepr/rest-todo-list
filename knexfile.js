998309c4bd081a90ecb2640f942c5d6e46409335
Arjun Raju Pillai <revarjunr@gmail.com>
2017-08-01 20:58:45 +0530

    Make the migrations more fail-proof.

---
+++ knexfile.js
@@ -0,0 +1,27 @@
+// We're using the env object from the process modules.
+// We'll be using it to access the environment variables in our .env file.
+require('dotenv').config();
+
+const { env } = require('process');
+
+const commonConfig = {
+  migrations: { tableName: 'db_migrations' },
+  seeds: { tableName: './seeders' },
+
+  client: 'mysql2',
+  connection: {
+    host: env.DB_HOST,
+
+    user: env.DB_USER,
+    password: env.DB_PASS,
+
+    // I know DB_DATABASE sounds redundant but still...
+    database: env.DB_DATABASE,
+    charset: env.DB_CHARSET
+  }
+};
+
+module.exports = {
+  development: commonConfig,
+  production: commonConfig 
+}
--- server.js
+++ server.js
@@ -1,1 +1,5 @@
 require('dotenv').config();
+
+// The death package is used to clean up our app 
+// when before it dies. Usually unexpectedly.
+const ON_DEATH = require('death')({uncaughtException: true});
